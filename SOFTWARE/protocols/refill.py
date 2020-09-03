b9f7fa7b19797f056b7416e2abd8fa92df5c9e5f
SariKerckhove <60110731+SariKerckhove@users.noreply.github.com>
2020-09-03 12:12:27 +0200

    Update refill.py

--- SOFTWARE/protocols/refill.py
+++ SOFTWARE/protocols/refill.py
@@ -9,16 +9,13 @@ plate = containers.load('96-well-plate-20mm', 'C3', 'plate')
 refillres = containers.load('point', 'D3', 'refillres')
 # Add a 200uL tip rack, and place it in slot 'B3'
 tiprack = containers.load('tiprack-200ul', 'B3','tiprack')
-# Add a trash container in slot 'B2'
-trash = containers.load('point', 'B2', 'trash')
 
 # pipettes
 # Add a 200uL 8 channel pipette to axis 'a', and tell it to use that tip rack and trash container
 pipette = instruments.Pipette(
     axis='a', # weird difference between a and b
     channels=8, 
-    max_volume=200, tip_racks=[tiprack],
-    trash_container=trash)
+    max_volume=200, tip_racks=[tiprack])
 
 ## PART II: Specify protocol
 # commands
